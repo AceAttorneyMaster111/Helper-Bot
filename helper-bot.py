8048da243d6b49f8e00dea02bd16ab175b3daa06
AceAttorneyMaster111 <NoahS2003@gmail.com>
2018-04-23 18:29:45 -0400

    Added "offline" to trigger words

--- helper-bot.py
+++ helper-bot.py
@@ -10,11 +10,11 @@ async def on_ready():
 @client.event
 async def on_message(message):
     lowercase = message.content.lower()
-    if ("toasty" in lowercase or "bot" in lowercase) and ("down" in lowercase or "not working" in lowercase or "dead" in lowercase) and message.author != client.user and message.channel.id == "303203727362490368":
+    if ("toasty" in lowercase or "bot" in lowercase) and ("down" in lowercase or "not working" in lowercase or "dead" in lowercase or "offline" in lowercase) and message.author != client.user and message.channel.id == "303203727362490368":
         await client.send_message(message.channel, message.author.mention + ", is Toasty down? When a bot admin comes by, they can restart the bot.")
 while True:
     try:
-        client.loop.run_until_complete(client.start("Bot token"))
+        client.loop.run_until_complete(client.start("Bot Token"))
     except Exception as e:
         print("Error: {}. Reconnecting.".format(str(type(e))))
     except BaseException as f:
