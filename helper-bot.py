17861fc0c0820d75d87d3d4878d76c1ea090f673
AceAttorneyMaster111 <NoahS2003@gmail.com>
2018-04-23 13:37:23 -0400

    Initial commit

---
+++ helper-bot.py
@@ -0,0 +1,23 @@
+import discord
+import asyncio
+client = discord.Client()
+@client.event
+async def on_ready():
+    print("Bot working.")
+    print(client.user.name + "#" + client.user.discriminator)
+    print(client.user.id)
+    print("------")
+@client.event
+async def on_message(message):
+    lowercase = message.content.lower()
+    if ("toasty" in lowercase or "bot" in lowercase) and ("down" in lowercase or "not working" in lowercase or "dead" in lowercase) and message.author != client.user and message.channel.id == "303203727362490368":
+        await client.send_message(message.channel, message.author.mention + ", is Toasty down? When a bot admin comes by, they can restart the bot.")
+while True:
+    try:
+        client.loop.run_until_complete(client.start("Bot token"))
+    except Exception as e:
+        print("Error: {}. Reconnecting.".format(str(type(e))))
+    except BaseException as f:
+        print("Exiting: " + str(type(f)))
+        client.loop.run_until_complete(client.logout())
+        exit()
